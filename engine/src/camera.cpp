eaacbb0304538d2f3ee459262d4279bf0d53bf1a
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:28:32 +0900

    Add world_generator and camera source files to engine library

--- engine/src/camera.cpp
+++ engine/src/camera.cpp
@@ -4,11 +4,6 @@
 Camera::Camera() : position(0.0f) {}
 Camera::Camera(const glm::vec3& pos) : position(pos) {}
 
-Camera::Camera(const glm::vec3& position) {
-    // 必要に応じて position をメンバ変数に格納
-    // 例: this->position = position;
-}
-
 void Camera::update(float deltaTime) {
-    // 実装例: カメラの移動や回転処理
+    // カメラの更新処理（必要に応じて実装）
 } 
