5eccf044c6427c0e3f84b6e629e60307fe37472c
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:06:40 +0900

    Add implementation of createShaderProgram method in Renderer class

--- engine/src/renderer.cpp
+++ engine/src/renderer.cpp
@@ -3,6 +3,52 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+GLuint Renderer::createShaderProgram(const char* vertexPath, const char* fragmentPath) {
+    // シェーダーファイルの読み込み
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+
+    vShaderFile.open(vertexPath);
+    fShaderFile.open(fragmentPath);
+    std::stringstream vShaderStream, fShaderStream;
+    vShaderStream << vShaderFile.rdbuf();
+    fShaderStream << fShaderFile.rdbuf();
+    vShaderFile.close();
+    fShaderFile.close();
+    vertexCode = vShaderStream.str();
+    fragmentCode = fShaderStream.str();
+
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+
+    // 頂点シェーダーのコンパイル
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vShaderCode, NULL);
+    glCompileShader(vertexShader);
+
+    // フラグメントシェーダーのコンパイル
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fShaderCode, NULL);
+    glCompileShader(fragmentShader);
+
+    // シェーダープログラムの作成
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    // シェーダーの削除
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
 
 void Renderer::init() {
     // シェーダーの初期化
