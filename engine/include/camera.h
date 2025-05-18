79e726b18682827194e89ef3c54a3763dcf2d2dd
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:13:44 +0900

    Add player attributes and initialization methods to Player class

--- engine/include/camera.h
+++ engine/include/camera.h
@@ -1,9 +1,14 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include <glm/glm.hpp>
+
 class Camera {
 public:
-    void update(float deltaTime) {}
+    glm::vec3 position;
+    Camera();
+    Camera(const glm::vec3& pos);
+    void update(float deltaTime);
 };
 
 #endif // CAMERA_H 
--- engine/include/player.h
+++ engine/include/player.h
@@ -1,9 +1,20 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
 class Player {
 public:
-    void update(float deltaTime) {}
+    glm::vec3 position;
+    glm::vec3 velocity;
+    float yaw, pitch;
+    GLFWwindow* window;
+    float lastMouseX, lastMouseY;
+
+    Player();
+    void init(GLFWwindow* window);
+    void update(float deltaTime);
 };
 
 #endif // PLAYER_H 
