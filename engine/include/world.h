f6aa5a84a25f857668b92301f8af27816c6a5db5
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:19:07 +0900

    Add terrain generation methods and noise functions to World class

--- engine/include/world.h
+++ engine/include/world.h
@@ -1,9 +1,27 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include "material.h"
+#include "particle.h"
+#include <random>
+#include <glm/glm.hpp>
+
 class World {
+private:
+    std::mt19937 generator;
+    int octaves;
+    float persistence;
+    float lacunarity;
+    float scale;
+
+    float getPerlinNoise(float x, float y, float z);
+    float fade(float t);
+    float grad(int hash, float x, float y, float z);
+
 public:
-    void update(float deltaTime) {}
+    World();
+    void generateTerrain(Material& terrain);
+    void update(float deltaTime);
 };
 
 #endif // WORLD_H 
