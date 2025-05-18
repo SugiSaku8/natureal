be68f6ed54656ef262da395a0fe8318e0ce1ee4f
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:34:42 +0900

    Add Game class header with member variables and methods for initialization and updates

--- game/include/game.h
+++ game/include/game.h
@@ -1,5 +1,30 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <GLFW/glfw3.h>
+#include "renderer.h"
+#include "world_generator.h"
+#include "material.h"
+#include "camera.h"
+#include "player.h"
+#include "world.h"
+
+class Game {
+private:
+    GLFWwindow* window;
+    Renderer renderer;
+    WorldGenerator worldGen;
+    Material terrain;
+    Camera camera;
+    Player player;
+    World world;
+    float lastFrame, currentFrame;
+
 public:
     Game(GLFWwindow* window);
     void init();
     void update(float deltaTime);
-    GLFWwindow* getWindow() const { return window; } 
+    GLFWwindow* getWindow() const { return window; }
+};
+
+#endif // GAME_H 
