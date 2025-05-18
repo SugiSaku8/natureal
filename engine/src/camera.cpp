528fb359d2c734f4c4d929de714f721f2534aadd
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:26:03 +0900

    Add constructor overload in Camera class to initialize position with glm::vec3

--- engine/src/camera.cpp
+++ engine/src/camera.cpp
@@ -1,7 +1,13 @@
 #include "camera.h"
+#include <glm/glm.hpp>
 
 Camera::Camera() : position(0.0f) {}
 Camera::Camera(const glm::vec3& pos) : position(pos) {}
+
+Camera::Camera(const glm::vec3& position) {
+    // 必要に応じて position をメンバ変数に格納
+    // 例: this->position = position;
+}
 
 void Camera::update(float deltaTime) {
     // 実装例: カメラの移動や回転処理
