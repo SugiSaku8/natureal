0f7b45bb8fb5780371c7286f1c81f33a2af6289b
Sugisaku8 <sumisj522@gmail.com>
2025-05-18 18:09:10 +0900

    Remove temporary object file for renderer

---
+++ engine/include/world_generator.h
@@ -0,0 +1,11 @@
+#ifndef WORLD_GENERATOR_H
+#define WORLD_GENERATOR_H
+
+#include "material.h"
+
+class WorldGenerator {
+public:
+    void generateWorld(Material& terrain);
+};
+
+#endif // WORLD_GENERATOR_H 
