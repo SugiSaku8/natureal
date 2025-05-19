29f92d3034f8c4e71d5d9df6132e42bc782289d6
Sugisaku8 <sumisj522@gmail.com>
2025-05-19 17:45:44 +0900

    Add rendering methods and initialize graphics API in Renderer class

--- src/renderer/Renderer.cpp
+++ src/renderer/Renderer.cpp
@@ -1,4 +1,5 @@
 #include "renderer/Renderer.hpp"
+#include <memory>
 
 namespace Natureal {
 Renderer::Renderer() {}
@@ -6,4 +7,13 @@ Renderer::~Renderer() {}
 bool Renderer::initialize() { return true; }
 void Renderer::beginFrame() {}
 void Renderer::endFrame() {}
+void Renderer::renderMesh(const Mesh&, const Material&, const glm::mat4&) {}
+void Renderer::setViewMatrix(const glm::mat4&) {}
+void Renderer::setProjectionMatrix(const glm::mat4&) {}
+std::shared_ptr<Shader> Renderer::loadShader(const std::string&, const std::string&) { return nullptr; }
+std::shared_ptr<Texture> Renderer::loadTexture(const std::string&) { return nullptr; }
+std::shared_ptr<Material> Renderer::createMaterial(const std::string&) { return nullptr; }
+void Renderer::initializeGraphicsAPI() {}
+void Renderer::createDefaultResources() {}
+void Renderer::setupRenderPass() {}
 } // namespace Natureal 
